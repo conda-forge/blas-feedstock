be7d193ea8a36b0819dce893dfe02eafc99f1878
Isuru Fernando <isuruf@gmail.com>
2019-01-07 01:17:46 -0600

    print error log

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,8 +8,8 @@ ren "C:\Program Files\Git\usr\bin\sh.exe" _sh.exe
 
 REM Link against the netlib libraries
 cmake -G"MinGW Makefiles" .. ^
-    "-DBLAS_LIBRARIES=%LIBRARY_LIB%/blas.lib;%LIBRARY_LIB%/cblas.lib" \
-    "-DLAPACK_LIBRARIES=%LIBRARY_LIB%/lapack.lib;%LIBRARY_LIB%/lapacke.lib" \
+    "-DBLAS_LIBRARIES=%LIBRARY_LIB%\blas.lib;%LIBRARY_LIB%\cblas.lib" \
+    "-DLAPACK_LIBRARIES=%LIBRARY_LIB%\lapack.lib;%LIBRARY_LIB%\lapacke.lib" \
     -DBUILD_TESTING=yes \
     -DCMAKE_BUILD_TYPE=Release \
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -11,7 +11,9 @@ cmake .. \
     "-DBLAS_LIBRARIES=${PREFIX}/lib/libblas${SHLIB_EXT};${PREFIX}/lib/libcblas${SHLIB_EXT}" \
     "-DLAPACK_LIBRARIES=${PREFIX}/lib/liblapack${SHLIB_EXT};${PREFIX}/lib/liblapacke${SHLIB_EXT}" \
     -DBUILD_TESTING=yes \
-    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_BUILD_TYPE=Release 
+
+cat CMakeFiles/CMakeOutput.log CMakeFiles/CMakeError.log
 
 make -j${CPU_COUNT}
 
