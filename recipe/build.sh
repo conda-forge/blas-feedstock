03fd0b1b57bd67ea5a39c76497da99284274a3a7
Isuru Fernando <isuruf@gmail.com>
2019-01-08 01:16:03 -0600

    Add CPATH and LIBRARY_PATH

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,13 +5,17 @@ mkdir build
 cd build
 
 ls -al ${PREFIX}/lib
+ls -al ${PREFIX}/include
+
+export CPATH="${CPATH}:${PREFIX}/include"
+export LIBRARY_PATH="${LIBRARY_PATH}:${PREFIX}/lib"
 
 # Link against the netlib libraries
 cmake .. \
     "-DBLAS_LIBRARIES=${PREFIX}/lib/libblas${SHLIB_EXT};${PREFIX}/lib/libcblas${SHLIB_EXT}" \
     "-DLAPACK_LIBRARIES=${PREFIX}/lib/liblapack${SHLIB_EXT};${PREFIX}/lib/liblapacke${SHLIB_EXT}" \
     -DBUILD_TESTING=yes \
-    -DCMAKE_BUILD_TYPE=Release 
+    -DCMAKE_BUILD_TYPE=Release
 
 cat CMakeFiles/CMakeOutput.log CMakeFiles/CMakeError.log
 
