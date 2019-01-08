744970758bdd6dba6e7b2449df99ba37b2ee7a6a
Isuru Fernando <isuruf@gmail.com>
2019-01-08 02:01:51 -0600

    Fix windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,12 +6,15 @@ dir %LIBRARY_LIB%
 REM Trick to avoid CMake/sh.exe error
 ren "C:\Program Files\Git\usr\bin\sh.exe" _sh.exe
 
+set CPATH="%CPATH%;%LIBRARY_INC%"
+set LIBRARY_PATH="%LIBRARY_PATH%;%LIBRARY_LIB%"
+
 REM Link against the netlib libraries
 cmake -G"MinGW Makefiles" .. ^
-    "-DBLAS_LIBRARIES=%LIBRARY_LIB%\blas.lib;%LIBRARY_LIB%\cblas.lib" \
-    "-DLAPACK_LIBRARIES=%LIBRARY_LIB%\lapack.lib;%LIBRARY_LIB%\lapacke.lib" \
-    -DBUILD_TESTING=yes \
-    -DCMAKE_BUILD_TYPE=Release \
+    "-DBLAS_LIBRARIES=blas.lib;cblas.lib" ^
+    "-DLAPACK_LIBRARIES=lapack.lib;lapacke.lib" ^
+    -DBUILD_TESTING=yes ^
+    -DCMAKE_BUILD_TYPE=Release
 
 mingw32-make -j%CPU_COUNT%
 
