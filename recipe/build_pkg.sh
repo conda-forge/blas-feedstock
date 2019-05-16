3d4a352c0d5de2e2762fd6bb4234397fd0b7e1e2
Sylvain Corlay <sylvain.corlay@gmail.com>
2019-05-16 22:20:18 +0200

    Add symlinks for .so files

--- recipe/build_pkg.sh
+++ recipe/build_pkg.sh
@@ -1,7 +1,9 @@
 if [[ "$(uname)" == "Darwin" ]]; then
     ln -s $PREFIX/lib/lib${blas_impl_lib}.dylib $PREFIX/lib/${PKG_NAME}.${PKG_VERSION:0:1}.dylib
+    ln -s $PREFIX/lib/lib${blas_impl_lib}.dylib $PREFIX/lib/${PKG_NAME}.dylib
 else
     ln -s $PREFIX/lib/lib${blas_impl_lib}.so $PREFIX/lib/${PKG_NAME}.so.${PKG_VERSION:0:1}
+    ln -s $PREFIX/lib/lib${blas_impl_lib}.so $PREFIX/lib/${PKG_NAME}.so
 fi
 
 if [[ "${blas_impl}" == "mkl" ]]; then
