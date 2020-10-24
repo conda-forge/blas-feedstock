c2f8adbba353901808641b5ea166cfaf4be81f04
Isuru Fernando <isuruf@gmail.com>
2020-10-24 14:18:06 -0500

    cmake -LAH

--- recipe/build.sh
+++ recipe/build.sh
@@ -35,7 +35,7 @@ conda${EXE_SUFFIX} create -p ${NEW_ENV} -c conda-forge --yes --quiet \
 rm -rf ${NEW_ENV}/lib/libgfortran.*dylib
 
 # Link against the netlib libraries
-cmake ${CMAKE_ARGS} -G "${CMAKE_GENERATOR}" .. \
+cmake ${CMAKE_ARGS} -LAH -G "${CMAKE_GENERATOR}" .. \
     "-DBLAS_LIBRARIES=${SHLIB_PREFIX}blas${SHLIB_EXT};${SHLIB_PREFIX}cblas${SHLIB_EXT}" \
     "-DLAPACK_LIBRARIES=${SHLIB_PREFIX}lapack${SHLIB_EXT};${SHLIB_PREFIX}lapacke${SHLIB_EXT}" \
     -DBUILD_TESTING=yes \
