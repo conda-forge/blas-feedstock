1663549f978efcbead5b60a0707f4d519cb3ae63
H. Vetinari <h.vetinari@gmx.com>
2020-11-15 18:06:16 +0100

    remove redundant skips; add skip for LAPACK-xeigtst?_svd_in on x86_64

--- recipe/test_blas.sh
+++ recipe/test_blas.sh
@@ -21,21 +21,27 @@ fi
 if [[ "$target_platform" == osx-* ]]; then
   # testing with shared libraries does not work. skip them.
   # to test that program exits if wrong parameters are given, what the testsuite
-  # do is that the symbol xerbla (xerbla logs the error and exits) is overriden
+  # does is that the symbol xerbla (which logs the error and exits) is overriden
   # by the test program's own version which reports to the test program that
   # xerbla was called. This does not work with dylibs on osx and dlls on windows
   SKIP_TESTS="${SKIP_TESTS}|x*cblat2|x*cblat3"
 fi
 
 if [[ "${blas_impl}" == "mkl" ]]; then
-  # These tests fail even when linked directly against mkl_rt. Skip for now
-  SKIP_TESTS="${SKIP_TESTS}|LAPACK-xeigtstz_sep_in|LAPACK-xeigtstz_zsb_in|LAPACK-xeigtstz_se2_in|LAPACK-xlintstrfz_ztest_rfp_in|LAPACK-xlintstz_ztest_in"
-  SKIP_TESTS="${SKIP_TESTS}|LAPACK-xlintstc_ctest_in|LAPACK-xlintstrfc_ctest_rfp_in|LAPACK-xeigtstc_sep_in|LAPACK-xeigtstc_se2_in|LAPACK-xeigtstc_ced_in"
-  SKIP_TESTS="${SKIP_TESTS}|LAPACK-xeigtstc_csb_in|LAPACK-xeigtstc_csg_in|LAPACK-xeigtstz_zed_in|LAPACK-xeigtstz_zsg_in|LAPACK-xlintstzc_zctest_in"
-  SKIP_TESTS="${SKIP_TESTS}|BLAS-xblat1c|BLAS-xblat1z"
-
-  if [[ "$target_platform" == linux* ]]; then
+  if [[ "$target_platform" == "linux-64" ]]; then
+    # TODO: figure out these segfaults
     SKIP_TESTS="${SKIP_TESTS}|example_DGELS_rowmajor|example_DGELS_colmajor"
+  elif [[ "$target_platform" == "osx-64" || "$target_platform" == "win-64" ]]; then
+    # "shared" failures on osx-64, win-64
+    SKIP_TESTS="${SKIP_TESTS}|BLAS-xblat1c|LAPACK-xeigtstc_ced_in|LAPACK-xeigtstc_csb_in|LAPACK-xeigtstc_csg_in"
+    SKIP_TESTS="${SKIP_TESTS}|LAPACK-xeigtstc_se2_in|LAPACK-xeigtstc_sep_in"
+    SKIP_TESTS="${SKIP_TESTS}|LAPACK-xlintstc_ctest_in|LAPACK-xlintstrfc_ctest_rfp_in"
+  fi
+  if [[ "$target_platform" == "osx-64" ]]; then
+    # additional failures only on osx-64
+    SKIP_TESTS="${SKIP_TESTS}|BLAS-xblat1z|LAPACK-xeigtstz_se2_in|LAPACK-xeigtstz_sep_in"
+    SKIP_TESTS="${SKIP_TESTS}|LAPACK-xeigtstz_zed_in|LAPACK-xeigtstz_zsb_in|LAPACK-xeigtstz_zsg_in"
+    SKIP_TESTS="${SKIP_TESTS}|LAPACK-xlintstrfz_ztest_rfp_in|LAPACK-xlintstz_ztest_in|LAPACK-xlintstzc_zctest_in"
   fi
 fi
 
@@ -48,10 +54,13 @@ if [[ "${blas_impl}" == "openblas" && "$(uname)" == MINGW* ]]; then
   SKIP_TESTS="${SKIP_TESTS}|LAPACK-xeigtstz_glm_in|LAPACK-xeigtstz_gsv_in|LAPACK-xeigtstz_lse_in|BLAS-xblat1c"
 fi
 
-if [[ ! "$(uname -m)" == "x86_64" ]]; then
+if [[ "$target_platform" == "linux-ppc64le" ]]; then
+  # lots of failing tests for this test-prefix on ppc
   SKIP_TESTS="${SKIP_TESTS}|LAPACK-xeigtstz"
+elif [[ "$target_platform" == *-64 ]]; then
+  # gained with MKL 2020.4 for lapack 3.9 across all x86_64 platforms, not sure why
+  SKIP_TESTS="${SKIP_TESTS}|LAPACK-xeigtstc_svd_in|LAPACK-xeigtstd_svd_in|LAPACK-xeigtsts_svd_in|LAPACK-xeigtstz_svd_in"
 fi
-
 
 if [[ "$target_platform" == "win-64" ]]; then
   ${BUILD_PREFIX}/Library/bin/ctest --output-on-failure -E "${SKIP_TESTS}"
