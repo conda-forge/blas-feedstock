8125f427d2c1141082d92c4225ac22b9012709fd
Isuru Fernando <isuruf@gmail.com>
2021-01-06 13:19:02 -0600

    fix other blas

--- recipe/install_blas_devel.sh
+++ recipe/install_blas_devel.sh
@@ -1,3 +1,5 @@
-ln -s $PREFIX/lib/pkgconfig/openblas.pc $PREFIX/lib/pkgconfig/blas.pc
-ln -s $PREFIX/lib/pkgconfig/openblas.pc $PREFIX/lib/pkgconfig/cblas.pc
-ln -s $PREFIX/lib/pkgconfig/openblas.pc $PREFIX/lib/pkgconfig/lapack.pc
+if [[ "$blas_impl" == "openblas" ]]; then
+  ln -s $PREFIX/lib/pkgconfig/openblas.pc $PREFIX/lib/pkgconfig/blas.pc
+  ln -s $PREFIX/lib/pkgconfig/openblas.pc $PREFIX/lib/pkgconfig/cblas.pc
+  ln -s $PREFIX/lib/pkgconfig/openblas.pc $PREFIX/lib/pkgconfig/lapack.pc
+fi
