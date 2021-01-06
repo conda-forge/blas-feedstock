9c8854fcca6350a4cfcc8736c177c5ab28160d64
Isuru Fernando <isuruf@gmail.com>
2021-01-06 12:44:28 -0600

    add blas.pc for openblas

---
+++ recipe/install_blas_devel.sh
@@ -0,0 +1,3 @@
+ln -s $PREFIX/lib/pkgconfig/openblas.pc $PREFIX/lib/blas.pc
+ln -s $PREFIX/lib/pkgconfig/openblas.pc $PREFIX/lib/cblas.pc
+ln -s $PREFIX/lib/pkgconfig/openblas.pc $PREFIX/lib/lapack.pc
