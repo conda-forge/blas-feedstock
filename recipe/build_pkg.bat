7be81a4e407cda6ad2bb6721a573dfd2278a3396
H. Vetinari <h.vetinari@gmx.com>
2021-01-07 22:56:58 +0100

    repackage lapack(e) for blis

--- recipe/build_pkg.bat
+++ recipe/build_pkg.bat
@@ -1,1 +1,24 @@
+REM batch is too stupid for or-conditions (much less nesting), so do it manually
+set "REPACK=F"
+if "%blas_impl%"=="blis" (
+    REM blis does not package liblapack(e); repack netlib-variant
+    if "%PKG_NAME%"=="liblapack" (
+        set "REPACK=T"
+    )
+    if "%PKG_NAME%"=="liblapacke" (
+        set "REPACK=T"
+    )
+)
+set "NEW_ENV=%cd%\\_env"
+if "%REPACK%"=="T" (
+    set "CONDA_SUBDIR=%target_platform%"
+    conda.exe create -p %NEW_ENV% -c conda-forge --yes --quiet ^
+        liblapack=%PKG_VERSION%=*netlib ^
+        liblapacke=%PKG_VERSION%=*netlib
+    set "CONDA_SUBDIR="
+    copy "%NEW_ENV%\\Library\\bin\\%PKG_NAME%.dll" "%LIBRARY_BIN%\\%PKG_NAME%.dll"
+    REM cut short the rest of the script
+    exit 0
+)
+
 copy "%LIBRARY_BIN%\\%blas_impl_lib%" "%LIBRARY_BIN%\\%PKG_NAME%.dll"
--- recipe/build_pkg.sh
+++ recipe/build_pkg.sh
@@ -1,14 +1,34 @@
-if [[ "$target_platform" == osx-* ]]; then
-    ln -s $PREFIX/lib/${blas_impl_lib} $PREFIX/lib/${PKG_NAME}.${PKG_VERSION:0:1}.dylib
-else
-    ln -s $PREFIX/lib/${blas_impl_lib} $PREFIX/lib/${PKG_NAME}.so.${PKG_VERSION:0:1}
-fi
-ln -s $PREFIX/lib/${blas_impl_lib} $PREFIX/lib/${PKG_NAME}${SHLIB_EXT}
-
 if [[ "${blas_impl}" == "mkl" ]]; then
     for CHANGE in "activate" "deactivate"
     do
         mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
         cp "${RECIPE_DIR}/libblas_mkl_${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/libblas_mkl_${CHANGE}.sh"
     done
+elif [[ "${blas_impl}" == "blis" ]]; then
+    # blis does not package liblapack(e); repack netlib-variant
+    if [[ "${PKG_NAME}" == "liblapack" || "${PKG_NAME}" == "liblapacke"  ]]; then
+        export NEW_ENV=`pwd`/_env
+        export CONDA_SUBDIR="${target_platform}"
+        conda create -p ${NEW_ENV} -c conda-forge --yes --quiet \
+            liblapack=${PKG_VERSION}=*netlib \
+            liblapacke=${PKG_VERSION}=*netlib
+        unset CONDA_SUBDIR
+        # copy once...
+        cp $NEW_ENV/lib/${PKG_NAME}${SHLIB_EXT} $PREFIX/lib/${PKG_NAME}${SHLIB_EXT}
+        # ... link the other
+        if [[ "$target_platform" == osx-* ]]; then
+            ln -s $PREFIX/lib/${PKG_NAME}${SHLIB_EXT} $PREFIX/lib/${PKG_NAME}.${PKG_VERSION:0:1}.dylib
+        else
+            ln -s $PREFIX/lib/${PKG_NAME}${SHLIB_EXT} $PREFIX/lib/${PKG_NAME}.so.${PKG_VERSION:0:1}
+        fi
+        # cut short the rest of the script
+        exit 0
+    fi
 fi
+
+if [[ "$target_platform" == osx-* ]]; then
+    ln -s $PREFIX/lib/${blas_impl_lib} $PREFIX/lib/${PKG_NAME}.${PKG_VERSION:0:1}.dylib
+else
+    ln -s $PREFIX/lib/${blas_impl_lib} $PREFIX/lib/${PKG_NAME}.so.${PKG_VERSION:0:1}
+fi
+ln -s $PREFIX/lib/${blas_impl_lib} $PREFIX/lib/${PKG_NAME}${SHLIB_EXT}
