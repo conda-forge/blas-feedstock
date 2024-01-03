574ce7621d0145dc244ee07f41f94ec7d48942dd
Isuru Fernando <isuruf@gmail.com>
2024-01-03 09:21:49 +0530

    fix test

--- recipe/test_blas.sh
+++ recipe/test_blas.sh
@@ -27,7 +27,7 @@ if [[ "$target_platform" == osx-* ]]; then
   SKIP_TESTS="${SKIP_TESTS}|x*cblat2|x*cblat3"
   # Not sure why the following tests work with other blas implementations, but
   # they check error codes as well
-  if [[ "${blas_impl}" == "accelerate" ]]; then
+  if [[ "${blas_impl}" == *"accelerate" ]]; then
     SKIP_TESTS="${SKIP_TESTS}|xblat2*|xblat3*"
   fi
 fi
