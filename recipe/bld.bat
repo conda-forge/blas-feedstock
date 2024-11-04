5990523ca8ae025a66346d5b71e9212c513677aa
H. Vetinari <h.vetinari@gmx.com>
2024-11-05 05:37:56 +1100

    update fortran compiler info also in `bld.bat`

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -13,8 +13,8 @@ set MSYSTEM=MINGW%ARCH%
 set MSYS2_PATH_TYPE=inherit
 set CHERE_INVOKING=1
 set "SHLIB_PREFIX="
-set "fortran_compiler=m2w64-toolchain"
-set "fortran_compiler_version=2"
+set "fortran_compiler=flang"
+set "fortran_compiler_version=19"
 bash -x "./build.sh"
 IF %ERRORLEVEL% NEQ 0 exit 1
 exit 0
