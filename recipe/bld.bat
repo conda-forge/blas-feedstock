b06aa41d26296383f9272291510a98956bcdf8da
H. Vetinari <h.vetinari@gmx.com>
2024-11-08 11:36:52 +1100

    don't forward to build.sh from bld.bat; use independent implementation

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,36 @@
-:: Trailing semicolon in this variable as set by current (2017/01)
-:: conda-build breaks us. Manual fix:
-set "MSYS2_ARG_CONV_EXCL=/AI;/AL;/OUT;/out"
-copy "%RECIPE_DIR%\build.sh" .
-set "SHLIB_EXT=.lib"
-set "CMAKE_GENERATOR=Ninja"
-set CHERE_INVOKING=1
-set "SHLIB_PREFIX="
-set "fortran_compiler=flang"
-set "fortran_compiler_version=19"
-bash -x "./build.sh"
-IF %ERRORLEVEL% NEQ 0 exit 1
-exit 0
+@echo on
+
+mkdir build
+cd build
+
+set "NEW_ENV=%cd%\_env"
+
+set "LIBRARY_PREFIX=%NEW_ENV%\Library"
+:: For finding cmake (not in NEW_ENV but actual build env)
+set "PATH=%PATH%:%BUILD_PREFIX%\Library\bin"
+
+set "CPATH=%LIBRARY_PREFIX%\include"
+set "LIBRARY_PATH==%LIBRARY_PREFIX%\lib"
+
+set "FFLAGS=-I%LIBRARY_PREFIX%\include %FFLAGS%"
+set "LDFLAGS=-L%LIBRARY_PREFIX%\lib %LDFLAGS%"
+
+%MINIFORGE_HOME%\Scripts\conda.exe create -p %NEW_ENV% -c conda-forge --yes --quiet ^
+    libblas=%PKG_VERSION%=*netlib ^
+    libcblas=%PKG_VERSION%=*netlib ^
+    liblapack=%PKG_VERSION%=*netlib ^
+    liblapacke=%PKG_VERSION%=*netlib ^
+    flang_win-64=%fortran_compiler_version%
+
+:: Link against the netlib libraries
+cmake -LAH -G Ninja .. ^
+    "-DBLAS_LIBRARIES=blas.lib;cblas.lib" ^
+    "-DLAPACK_LIBRARIES=lapack.lib;lapacke.lib" ^
+    -DBUILD_TESTING=yes ^
+    -DCMAKE_BUILD_TYPE=Release
+if %ERRORLEVEL% neq 0 (type .\CMakeFiles\CMakeError.log && type .\CMakeFiles\CMakeOutput.log && exit 1)
+
+cmake --build . --config Release
+if %ERRORLEVEL% neq 0 exit 1
+
+rmdir /s /q %NEW_ENV%
