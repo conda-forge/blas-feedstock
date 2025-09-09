600d42e0b11e54055f38da4b72d6d6b010b13afd
Daniel Ching <dching@nvidia.com>
2025-09-09 21:12:24 +0000

    BUG: Remove out of date changes

--- recipe/build_pkg.sh
+++ recipe/build_pkg.sh
@@ -8,15 +8,6 @@ if [[ "$blas_impl" == *"accelerate" && "${PKG_NAME}" == "libblas" ]]; then
     cp $SRC_DIR/accelerate/* $PREFIX/lib/
 fi
 
-# NVPL BLAS has separate entrypoints for BLAS and LAPACK
-if [[ "$blas_impl" == "nvpl"* ]]; then
-    if [[ "${PKG_NAME}" == *"blas" ]]; then
-        export blas_impl_lib=${blas_impl_lib%%,*}
-    elif [[ "${PKG_NAME}" == "liblapack"* ]]; then
-        export blas_impl_lib=${blas_impl_lib#*,}
-    fi
-fi
-
 if [[ "$target_platform" == osx-* ]]; then
     ln -s $PREFIX/lib/${impl_lib} $PREFIX/lib/${PKG_NAME}.${PKG_VERSION:0:1}.dylib
 else
